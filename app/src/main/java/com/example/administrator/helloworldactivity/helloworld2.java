19b122a293441dd54407cae09ac10f557f62969e
my99075 <491493327@qq.com>
2019-09-21 01:16:32 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/helloworldactivity/helloworld2.java
@@ -0,0 +1,13 @@
+package com.example.administrator.helloworldactivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class helloworld2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.helloworld2_layout);
+    }
+}
